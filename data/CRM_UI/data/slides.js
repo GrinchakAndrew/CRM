0424bbf35940a66622ab4726d47b141a57f12712
GrinchakAndrew <andrewgrinchak@gmail.com>
2017-06-16 17:11:47 +0300

    init

---
+++ data/CRM_UI/data/slides.js
@@ -0,0 +1,15 @@
+{
+	"collection" : ["data/CRM_UI/data/slide1.html",
+					"data/CRM_UI/data/slide2.html", 
+					"data/CRM_UI/data/slide3.html", 
+					"data/CRM_UI/data/slide4.html", 
+					"data/CRM_UI/data/slide5.html", 
+					"data/CRM_UI/data/slide6.html", 
+					"data/CRM_UI/data/slide7.html", 
+					"data/CRM_UI/data/slide8.html",
+					"data/CRM_UI/data/slide9.html",
+					"data/CRM_UI/data/slide10.html",
+					"data/CRM_UI/data/slide11.html",
+					"data/CRM_UI/data/slide12.html"
+					]
+}
---
+++ data/tasks.js
@@ -0,0 +1,15 @@
+{
+	"(1) CookStream Brief " : [
+		{"User-Scenarios" : [
+					{"User-Scenarios by UI" : {"(1) UI Description" : "data/CRM_UI/UI.html"}}
+				  ]
+		}, 
+		{"Estimations" : [
+					{"PHP" : {"(1) PHP CookStream Estimates" : "data/CookStream_Estimations_PHP.txt"}}
+				  ]
+		}
+	]
+}
+
+
+
