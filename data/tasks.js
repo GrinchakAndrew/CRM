e9b158d917ff6670293af459f6d7249d4b2c423c
GrinchakAndrew <andrewgrinchak@gmail.com>
2017-09-11 15:31:40 +0300

    additional tasks added

--- data/tasks.js
+++ data/tasks.js
@@ -1,11 +1,12 @@
 {
-	"(1) CookStream Brief " : [
+	"(1) Easy2Give CRM Brief " : [
 		{"User-Scenarios" : [
 					{"User-Scenarios by UI" : {"(1) UI Description" : "data/CRM_UI/UI.html"}}
 				  ]
 		}, 
 		{"Estimations" : [
-					{"PHP" : {"(1) PHP CookStream Estimates" : "data/CookStream_Estimations_PHP.txt"}}
+					{"PHP + Additional Tasks" : {"(2) Additional Tasks" : "data/CRM_Additional_Tasks_Estimations.txt", 
+												 "(1) PHP CRM Estimates" : "data/CookStream_Estimations_PHP.txt"}}
 				  ]
 		}
 	]
